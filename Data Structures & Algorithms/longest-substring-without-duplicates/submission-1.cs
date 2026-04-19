6840887a7b5a2e0664777b756df5943e83798b52
Yehor <58122300+donatellony@users.noreply.github.com>
2026-04-19 09:52:22 +0200

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) 
+    {
+        HashSet<char> chars = [];
+        int l = 0, max = 0;
+        for(int r = 0; r < s.Length; r++)
+        {
+            while(chars.Contains(s[r]))
+            {
+                chars.Remove(s[l++]);
+            }
+            chars.Add(s[r]);
+            max = Math.Max(max, chars.Count);
+        }
+
+        return max;
+    }
+}
